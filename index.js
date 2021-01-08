732478856c842e3916bfb504354a6e085ea4cc5f
ryan cox <ryancox101@gmail.com>
2021-01-07 20:14:07 -0500

    add frontend build to repo

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqStr'))
 app.use(cors())
 
