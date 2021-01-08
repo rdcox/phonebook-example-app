a3c14ce74a981b79a7cc45645d1f6a96281faa28
ryan cox <ryancox101@gmail.com>
2021-01-07 19:54:25 -0500

    fixing heroku port

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.get('/info', (request, response) => {
 })
 
 // initialize express app on the given port
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
